70ba645bddd41ee1bf6545f80f49f9a3025dda5e
Hritvik Mohan <hritvik5005@gmail.com>
2023-04-23 15:59:00 +0530

    Add: counter reset button

--- src/apps/counter/Counter.jsx
+++ src/apps/counter/Counter.jsx
@@ -13,12 +13,19 @@ export default function Counter() {
             setCount(count - 1);
     }
 
+    function reset() {
+        setCount(0);
+    }
+
     return (
-        <div className="counter">
-            <p>Count: {count}</p>
-            <div>
-                <button className="counter-button" onClick={decrement}>Decrement</button>
-                <button className="counter-button" onClick={increment}>Increment</button>
+        <div className="counter-container">
+            <div className="counter">
+                <p>Count: {count}</p>
+                <div>
+                    <button className="counter-button" onClick={increment}>Increment</button>
+                    <button className="counter-button" onClick={decrement}>Decrement</button>
+                    <button className="counter-button" onClick={reset}>Reset</button>
+                </div>
             </div>
         </div>
     ) 
